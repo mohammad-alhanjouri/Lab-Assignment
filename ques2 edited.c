f654d9b117dc8aceda5feb2d7a4d39167f15ace0
Mohammed M. Al Hanjouri <117040263+mohammad-alhanjouri@users.noreply.github.com>
2022-11-02 18:28:33 +0200

    Add files via upload

---
+++ ques2 edited.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+	int a=1,b=5;
+	printf ("Group 1:\n");
+	while (a<=4){
+	printf ("%d",a);
+	a++;
+	}
+	printf ("\nGroup 2:\n");
+	while (b<=10){
+	printf ("%d",b);
+	b++;
+    }  
+	
+}
